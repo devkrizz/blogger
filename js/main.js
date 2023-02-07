f10302856a82a449da33f54b36ec577a4dbaa982
Krishnadev <krizzdev@gmail.com>
2023-02-07 10:18:24 +0530

    final file

---
+++ js/main.js
@@ -0,0 +1,32 @@
+
+
+$(document).ready(function(){
+
+    $nav=$('.nav');
+    $toggleCollapse= $('.toggle-collapse');
+
+    /** click event on toggle menu **/
+    $toggleCollapse.click(function(){
+        $nav.toggleClass('collapse');
+    })
+
+    //----Owl Carousel for blog----//
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        autoplay:false,
+        autoplayTimeout:3000,
+        dots:false,
+        nav:true,
+        navText:[$('.owl-navigation .owl-nav-prev'),$('.owl-navigation .owl-nav-next')]
+        
+    });
+
+    //click to scroll up
+    $('.move-up span').click(function(){
+        $('html,body').animate({
+            scrollTop: 0
+        },1000);
+    })
+
+});
